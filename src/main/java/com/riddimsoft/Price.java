217ef38a16048d06e3d422ac70bb23e12cec49c3
Marcin <marcin.drobiecki@gmail.com>
2013-08-09 19:27:43 +0200

    Price class now use proper value validation

--- src/main/java/com/riddimsoft/Price.java
+++ src/main/java/com/riddimsoft/Price.java
@@ -3,14 +3,10 @@ package com.riddimsoft;
 import com.riddimsoft.exceptions.BadPriceException;
 
 public final class Price {
-    private static final double MAX_ALLOWED_FLOAT_DIFFERENCE = 0.00001;
-
     private final float value;
 
     public Price(final float value) throws BadPriceException {
-        // TODO - zalozenie ze kolejne nominaly sa mnoznikami jest niesluszne
-        if (Math.abs((value / Coin.getMinimalDenomination()) - Math.round(value / Coin.getMinimalDenomination()))
-                > MAX_ALLOWED_FLOAT_DIFFERENCE) {
+        if (!Coin.canExpressValueWithAvailableDenominations(value)) {
             throw new BadPriceException();
         }
 
