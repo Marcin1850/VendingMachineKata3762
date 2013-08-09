623aaa98083570485b261e498f90cf94a9623bbe
Marcin <marcin.drobiecki@gmail.com>
2013-08-09 19:25:23 +0200

    Method canExpressValueWithAvailableDenominations added to Coin class

--- src/main/java/com/riddimsoft/Coin.java
+++ src/main/java/com/riddimsoft/Coin.java
@@ -1,15 +1,16 @@
 package com.riddimsoft;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 import com.riddimsoft.exceptions.NonExistentCoinException;
 
 public final class Coin {
-    private static final HashSet<Float> POSSIBLE_VALUES =
-            new HashSet<Float>(Arrays.asList(5.0f, 2.0f, 1.0f, 0.5f, 0.2f, 0.1f));
-    private static final float MINIMAL_DENOMINATION = Collections.min(POSSIBLE_VALUES);
+    private static final NavigableSet<Float> POSSIBLE_VALUES =
+            new TreeSet<Float>(Arrays.asList(5.0f, 2.0f, 1.0f, 0.5f, 0.2f, 0.1f));
+    private static final float MAX_ALLOWED_FLOAT_DIFFERENCE = 0.00001f;
 
     private final float value;
 
@@ -25,7 +26,29 @@ public final class Coin {
         return this.value;
     }
 
+    public static NavigableSet<Float> getPossibleValues() {
+        return POSSIBLE_VALUES;
+    }
+
     public static float getMinimalDenomination() {
-        return MINIMAL_DENOMINATION;
+        return POSSIBLE_VALUES.first();
+    }
+
+    public static boolean canExpressValueWithAvailableDenominations(final float value) {
+        float v = value;
+
+        for (final Iterator<Float> iter = POSSIBLE_VALUES.descendingIterator(); iter.hasNext();) {
+            final Float denominator = iter.next();
+
+            while (v - denominator > -MAX_ALLOWED_FLOAT_DIFFERENCE) {
+                v -= denominator;
+
+                if (Math.abs(v) < MAX_ALLOWED_FLOAT_DIFFERENCE) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
--- src/test/java/com/riddimsoft/CoinTest.java
+++ src/test/java/com/riddimsoft/CoinTest.java
@@ -1,7 +1,12 @@
 package com.riddimsoft;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.Iterator;
+import java.util.NavigableSet;
 
 import org.junit.Test;
 
@@ -11,6 +16,7 @@ public class CoinTest {
     private static final int EXISTENT_COIN_VALUE = 2;
     private static final float NON_EXISTENT_COIN_VALUE = 0.51f;
     private static final double FLOAT_COMPARISON_DELTA = 0.001;
+    private static final int NUMBER_OF_FIRST_DENOMINATORS = 3;
 
 
     @Test
@@ -30,5 +36,44 @@ public class CoinTest {
     @Test(expected = NonExistentCoinException.class)
     public void testCoinCreateNonExistent() throws NonExistentCoinException {
         new Coin(NON_EXISTENT_COIN_VALUE);
+    }
+
+    @Test
+    public void testCanExpressValueWithAvailableDenominationsPositive() {
+        final NavigableSet<Float> possibleValues = Coin.getPossibleValues();
+        float value = 0f;
+
+        int i = 0;
+        for (final Iterator<Float> iter = possibleValues.iterator();
+                iter.hasNext() && (i < NUMBER_OF_FIRST_DENOMINATORS); i++) {
+            value += iter.next();
+        }
+
+        assertTrue(Coin.canExpressValueWithAvailableDenominations(value));
+    }
+
+    @Test
+    public void testCanExpressValueWithAvailableDenominationsNagative() {
+        final NavigableSet<Float> possibleValues = Coin.getPossibleValues();
+        float value = 0f;
+
+        int i = 0;
+        for (final Iterator<Float> iter = possibleValues.iterator();
+                iter.hasNext() && (i < NUMBER_OF_FIRST_DENOMINATORS); i++) {
+            value += iter.next();
+        }
+        value += Coin.getMinimalDenomination() / 2;
+
+        assertFalse(Coin.canExpressValueWithAvailableDenominations(value));
+    }
+
+    @Test
+    public void testCanExpressValueWithAvailableDenominationsWithZero() {
+        assertFalse(Coin.canExpressValueWithAvailableDenominations(0));
+    }
+
+    @Test
+    public void testCanExpressValueWithAvailableDenominationsWithNegativeNumber() {
+        assertFalse(Coin.canExpressValueWithAvailableDenominations(-1));
     }
 }
