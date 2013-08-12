ba64528a3352a9c56fb8aaa70d01854bce784c55
Marcin <marcin.drobiecki@gmail.com>
2013-08-12 10:43:20 +0200

    DisplayTest fixed after class change

--- src/test/java/com/riddimsoft/DisplayTest.java
+++ src/test/java/com/riddimsoft/DisplayTest.java
@@ -11,25 +11,24 @@ public class DisplayTest {
     public final void testResetValue() throws DisplayException {
         final Display display = new Display();
 
-        display.addValue((float) Math.PI);
+        display.setValue((float) Math.PI);
         display.resetValue();
 
         assertEquals("0,00", display.getValue());
     }
 
     @Test
-    public final void testAddValues() throws DisplayException {
+    public final void testSetValue() throws DisplayException {
         final Display display = new Display();
 
-        display.addValue((float) Math.PI);
-        display.addValue((float) Math.PI);
+        display.setValue((float) Math.PI);
 
-        assertEquals(String.format("%.2f", 2 * Math.PI), display.getValue());
+        assertEquals(String.format("%.2f", Math.PI), display.getValue());
     }
 
     @Test(expected = DisplayException.class)
     public final void testAddNagativeNumber()
             throws DisplayException {
-        new Display().addValue(-1);
+        new Display().setValue(-1);
     }
 }
